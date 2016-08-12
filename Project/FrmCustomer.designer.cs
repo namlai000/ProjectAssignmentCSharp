718d6ac92afa324f9761b7e7f8753ef7b1337263
Dragon Of Justice <theguncrusher@gmail.com>
2016-08-12 17:48:50 +0700

    Fix groupbox text

--- Project/FrmCustomer.designer.cs
+++ Project/FrmCustomer.designer.cs
@@ -315,7 +315,7 @@
             this.groupBox1.Size = new System.Drawing.Size(290, 82);
             this.groupBox1.TabIndex = 28;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Text = "Search";
             // 
             // comboBox1
             // 

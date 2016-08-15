79a1e167eb56625e81ef924fa809c3d16b88d675
DragonOfJustice <theguncrusher@gmail.com>
2016-08-15 15:08:40 +0700

    Up

---
+++ Project/FrmProduct.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class FrmProduct : Form
+    {
+        private TSQLFundamentals2008Entities Entity = new TSQLFundamentals2008Entities();
+        Supplier sp = new Supplier();
+        Product pro = new Product();
+        public FrmProduct()
+        {
+            InitializeComponent();
+            cbSupplier.DataSource = Entity.Suppliers.ToList();
+            cbSupplier.ValueMember = "supplierid";
+            cbSupplier.DisplayMember = "companyname";
+            cbCate.DataSource = Entity.Categories.ToList();
+            cbCate.ValueMember = "categoryid";
+            cbCate.DisplayMember = "categoryname";
+            loadData();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'tSQLFundamentals2008DataSet.Products' table. You can move, or remove it, as needed.
+        }
+        bool validateInput()
+        {
+            string name = txtName.Text;
+           
+            if (name.Equals(""))
+            {
+                errorProvider1.SetError(txtName, "Please Input Product Name");
+            }
+            
+            return true;
+        }
+        void addNewProduct()
+        {
+            Product pro = new Product();
+            
+            pro.productname = txtName.Text;
+            int supplierid = int.Parse(cbSupplier.SelectedValue.ToString());
+            pro.supplierid = supplierid;
+            pro.categoryid = int.Parse(cbCate.SelectedValue.ToString());
+            pro.unitprice = decimal.Parse(txtPrice.Text);
+            if (chkDiscon.Checked)
+            {
+                pro.discontinued = true;
+            }
+            else pro.discontinued = false;
+            Entity.Products.Add(pro);
+            
+            Entity.SaveChanges();
+        }
+        void loadData()
+        {
+            dataGridView1.DataSource = Entity.Products.ToList();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                addNewProduct();
+                MessageBox.Show("Add Successfull");
+                loadData();
+              
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        void updateProduct()
+        {
+            string productid = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            int id = int.Parse(productid);
+            int supplierid = int.Parse(cbSupplier.SelectedValue.ToString());
+            Product pro = Entity.Products.Single(Product => Product.productid == id);
+            pro.productname = txtName.Text;
+
+            pro.supplierid = supplierid;
+            pro.categoryid = int.Parse(cbCate.SelectedValue.ToString());
+
+            pro.unitprice = decimal.Parse(txtPrice.Text);
+            if (chkDiscon.Checked)
+            {
+                pro.discontinued = true;
+            }
+            else pro.discontinued = false;
+            Entity.SaveChanges();
+            loadData();
+            MessageBox.Show("Update Successfull");
+
+        }
+        void deleteProduct()
+        {
+            DataGridViewRow r = dataGridView1.SelectedRows[0];
+            if (dataGridView1.Rows.Count > 0)
+            {
+                Product pro = null;
+                foreach (Product p in Entity.Products)
+                {
+                    if (p.productid == (int)r.Cells[0].Value)
+                    {
+                        pro = p;
+                        break;
+                    }
+                }
+                Entity.Products.Remove(pro);
+                Entity.SaveChanges();
+                loadData();
+                MessageBox.Show("Delete Successfull");
+            }
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                
+                DataGridViewRow r = dataGridView1.SelectedRows[0];
+                cbSupplier.SelectedValue = int.Parse(r.Cells[2].Value.ToString());
+                txtName.Text = r.Cells[1].Value.ToString();
+                cbCate.SelectedValue = int.Parse(r.Cells[3].Value.ToString());
+                txtPrice.Text = r.Cells[4].Value.ToString();
+                if (r.Cells[5].Value.ToString().Equals("True"))
+                {
+                    chkDiscon.Checked = true;
+                }
+                else chkDiscon.Checked = false;
+
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            updateProduct();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            deleteProduct();
+        }
+        void searchByName()
+        {
+            SearchForm sf = new SearchForm();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                DataGridViewRow r = dataGridView1.Rows[i];
+                if(r.Cells[1].Value.ToString().Equals(txtSearch.Text))
+                {
+                    
+                }
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            string sql=string.Format("Select * From Production.Products Where productname Like '%{0}%'",txtSearch.Text);
+            List<Product> list = Entity.Database.SqlQuery<Product>(sql).ToList();
+            dataGridView1.DataSource = list;
+        }
+    }
+}
---
+++ Project/SearchForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Project
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.productid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.supplierid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.categoryid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitprice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.discontinued = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productid,
+            this.productname,
+            this.supplierid,
+            this.categoryid,
+            this.unitprice,
+            this.discontinued});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(649, 349);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // productid
+            // 
+            this.productid.HeaderText = "ID";
+            this.productid.Name = "productid";
+            // 
+            // productname
+            // 
+            this.productname.HeaderText = "Name";
+            this.productname.Name = "productname";
+            // 
+            // supplierid
+            // 
+            this.supplierid.HeaderText = "Supplier ID";
+            this.supplierid.Name = "supplierid";
+            // 
+            // categoryid
+            // 
+            this.categoryid.HeaderText = "Category ID";
+            this.categoryid.Name = "categoryid";
+            // 
+            // unitprice
+            // 
+            this.unitprice.HeaderText = "Price";
+            this.unitprice.Name = "unitprice";
+            // 
+            // discontinued
+            // 
+            this.discontinued.HeaderText = "Discontinued";
+            this.discontinued.Name = "discontinued";
+            // 
+            // SearchForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 373);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SearchForm";
+            this.Text = "SearchForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn supplierid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitprice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn discontinued;
+    }
+}
